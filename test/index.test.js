b40d86c5a4b0ea0cfd546c8e3492813fa2daa556
lucky ZHOU <lucky.zhou@xplusz.com>
2016-05-07 20:56:54 +0800

    add coverage

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+import { doGet, doPut, doDelete, doPost, setConfig } from '../src';
+import chai from 'chai';
+
+const assert = chai.assert;
+
+describe('index', function() {
+
+    it('should return true', function() {
+        assert.equal(1, 1);
+    });
+
+});
