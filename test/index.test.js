c281c57c00eac98ac28fd84ad7b61419a472fe27
lucky ZHOU <lucky.zhou@xplusz.com>
2016-05-07 21:01:50 +0800

    fix lint issue

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,18 @@
+'use strict';
+
 import { doGet, doPut, doDelete, doPost, setConfig } from '../src';
 import chai from 'chai';
 
 const assert = chai.assert;
 
 describe('index', function() {
-
     it('should return true', function() {
+        doGet();
+        doPut();
+        doPost();
+        doDelete();
+        setConfig();
+
         assert.equal(1, 1);
     });
-
 });
