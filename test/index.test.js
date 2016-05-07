56bc382bff23794088f13c32db7c2aa9149bec36
lucky ZHOU <lucky.zhou@xplusz.com>
2016-05-07 21:35:12 +0800

    test coverall

--- test/index.test.js
+++ test/index.test.js
@@ -6,11 +6,31 @@ import chai from 'chai';
 const assert = chai.assert;
 
 describe('index', function() {
-    it('should return true', function() {
+    it('should get successfully', function() {
         doGet('url');
+
+        assert.equal(1, 1);
+    });
+
+    it('should put successfully', function() {
         doPut('url');
+
+        assert.equal(1, 1);
+    });
+
+    it('should post successfully', function() {
         doPost('url');
+
+        assert.equal(1, 1);
+    });
+
+    it('should delete successfully', function() {
         doDelete('url');
+
+        assert.equal(1, 1);
+    });
+
+    it('should setConfig successfully', function() {
         setConfig('url');
 
         assert.equal(1, 1);
